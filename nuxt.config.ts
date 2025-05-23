31bd66c5e8973b57811504b0602049f76e311fad
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-23 17:34:50 +0300

    feat: setting up prime vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import Aura from '@primeuix/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/icon', '@nuxt/eslint']
+  modules: [
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/eslint',
+    '@primevue/nuxt-module'
+  ], 
+  primevue: {
+    autoImport: true,
+    options: {
+        theme: {
+            preset: Aura
+        }
+    }
+},
+  runtimeConfig: {
+    public: {
+      restApiUrl: process.env.NUXT_PUBLIC_REST_API_URL ,
+    }
+  }
 })
