17910c05caa1250b3e6fbfa9c3f9769e5a217bf4
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-24 12:50:45 +0300

    fix: update login return type to AuthSessionSchema and improve formatting in login.vue

--- repositories/auth/index.ts
+++ repositories/auth/index.ts
@@ -1,11 +1,11 @@
 import type { NitroFetchRequest, $Fetch } from "nitropack";
 
-import type { User } from "~/types/login";
+import type { AuthSessionSchema, User } from "~/types/login";
 import type { LoginDto } from "./dto/login.dto";
 
 export const authRepository =<T> (fetch: $Fetch<T, NitroFetchRequest>) => ({
-    async login(loginDto: LoginDto): Promise<User[]> {
-        return fetch<User[]>('auth/login', {
+    async login(loginDto: LoginDto): Promise<AuthSessionSchema> {
+        return fetch<AuthSessionSchema>('auth/login', {
             method: 'POST', 
             body: loginDto
         })
