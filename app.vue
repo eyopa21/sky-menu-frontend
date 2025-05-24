282d0b50462300f45d39ef78e1adea5faf74aeb0
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-24 09:55:08 +0300

    feat: landing and login page done

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 
 <template>
-       <div>
-        <NuxtPage />
+       <div class="!min-h-screen">
+              <NuxtLayout>
+
+                     <NuxtPage />
+              </NuxtLayout>
        </div>
 </template>
