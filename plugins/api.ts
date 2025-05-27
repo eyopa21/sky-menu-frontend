0cd143cae975bac46aed8fd1a2b8dd9bfb996c68
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-27 11:02:55 +0300

    feat: add an api interceptor that adds the bearer token

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,11 +1,26 @@
 export default defineNuxtPlugin({
     name: 'api',
-    setup() {
+    dependsOn: ['authentication'],
+    async setup() {
         const config = useRuntimeConfig()
         const baseURL = config.public.restApiUrl
-
+        const { $authentication } =  useNuxtApp()
+        const accessToken = $authentication?.accessToken.value
         const api = $fetch.create({
             baseURL, 
+           
+            onRequest({ request, options, error }) {
+                if (accessToken) {
+                  options.headers.set('Authorization', `Bearer ${accessToken}`)
+                }
+              },
+
+              async onResponseError({ response }) {
+                if (response.status === 401) {
+                    console.log('Unauthorized access - redirecting to login')
+                //   await nuxtApp.runWithContext(() => navigateTo('/login'))
+                }
+              }
         })
         return {
             provide: {
